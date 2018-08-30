b083fc9fc455c22e7a05423702f45caac7609f39
Magnus Sebastian Christensen <magnussc92@hotmail.com>
2018-08-30 15:58:45 +0200

    stuff

--- 1D_Darkfield_mapping.py
+++ 1D_Darkfield_mapping.py
@@ -8,17 +8,29 @@ exec(open("1D_Darkfield_mapping.py").read(), globals())
 
 # import Functions
 import importlib
+import numpy as np
 import os
 import Functions as F # Separate script in the 'ESRF_ID06' folder.
 importlib.reload(F)
 
 master_folder = os.getcwd()
-print(master_folder)
 
+# Load background images.
 background_folder = os.path.join(master_folder, 'Backgrounds')
-files, filenames = F.loadFolder(background_folder, DataType='.edf')
+BG_files, BG_filenames = F.loadFolder(background_folder,
+                                      DataType='.edf', Mute=True)
 
-F.displayFiles(files, filenames, fps=2, Mute=False)
+# Convert background images to numpy.array
+BG_array = np.zeros((1,2,len(BG_filess)))
+print(np.shape(BG_array))
+
+# Load 1D data images.
+# oneD_data_folder = os.path.join(master_folder, 'Mosa_chi_scan_RT')
+# oneD_data_files, oneD_data_filenames = F.loadFolder(oneD_data_folder,
+#                                       DataType='.edf', Mute=True)
+
+# F.displayFiles(oneD_data_files, oneD_data_filenames,
+#                fps=2, Mute=False)
 
 F.closeFiles(Files=files)
 # loadFolder()
